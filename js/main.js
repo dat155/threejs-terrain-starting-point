4758e1a681015abe29cbbc9c8b32037b5373214c
oskarbraten <oskar.braten@outlook.com>
2020-10-11 22:33:58 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,247 @@
+import {
+    PerspectiveCamera,
+    WebGLRenderer,
+    PCFSoftShadowMap,
+    Scene,
+    Mesh,
+    TextureLoader,
+    RepeatWrapping,
+    DirectionalLight,
+    Vector3,
+    AxesHelper,
+} from './lib/three.module.js';
+
+import Utilities from './lib/Utilities.js';
+import MouseLookController from './controls/MouseLookController.js';
+
+import TextureSplattingMaterial from './materials/TextureSplattingMaterial.js';
+import TerrainBufferGeometry from './terrain/TerrainBufferGeometry.js';
+import { GLTFLoader } from './loaders/GLTFLoader.js';
+import { SimplexNoise } from './lib/SimplexNoise.js';
+
+async function main() {
+
+    const scene = new Scene();
+
+    const axesHelper = new AxesHelper(15);
+    scene.add(axesHelper);
+
+    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const renderer = new WebGLRenderer({ antialias: true });
+    renderer.setClearColor(0xffffff);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = PCFSoftShadowMap;
+
+    /**
+     * Handle window resize:
+     *  - update aspect ratio.
+     *  - update projection matrix
+     *  - update renderer size
+     */
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }, false);
+
+    /**
+     * Add canvas element to DOM.
+     */
+    document.body.appendChild(renderer.domElement);
+
+    /**
+     * Camera inital orientation:
+     */
+    camera.position.z = 70;
+    camera.position.y = 55;
+    camera.rotation.x -= Math.PI * 0.25;
+
+
+    /**
+     * Add terrain:
+     * 
+     * We have to wait for the image file to be loaded by the browser.
+     * There are many ways to handle asynchronous flow in your application.
+     * We are using the async/await language constructs of Javascript:
+     *  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+     */
+    const heightmapImage = await Utilities.loadImage('resources/images/heightmap.png');
+
+
+
+    /**
+     * Add trees:
+     */
+
+    const loader = new GLTFLoader();
+    loader.load(
+        // resource URL
+        'resources/models/kenney_nature_kit/tree_thin.glb',
+        // called when resource is loaded
+        (object) => {
+            for (let x = -50; x < 50; x += 8) {
+                for (let z = -50; z < 50; z += 8) {
+                    
+                    // TODO: Uncomment this once you've implemented the terrain.
+
+                    // const px = x + 1 + (6 * Math.random()) - 3;
+                    // const pz = z + 1 + (6 * Math.random()) - 3;
+
+                    // const height = terrainGeometry.getHeightAt(px, pz);
+
+                    // if (height < 5) {
+                    //     const tree = object.scene.children[0].clone();
+
+                    //     tree.traverse((child) => {
+                    //         if (child.isMesh) {
+                    //             child.castShadow = true;
+                    //             child.receiveShadow = true;
+                    //         }
+                    //     });
+                        
+                    //     tree.position.x = px;
+                    //     tree.position.y = height - 0.01;
+                    //     tree.position.z = pz;
+
+                    //     tree.rotation.y = Math.random() * (2 * Math.PI);
+
+                    //     tree.scale.multiplyScalar(1.5 + Math.random() * 1);
+
+                    //     scene.add(tree);
+                    // }
+
+                }
+            }
+        },
+        (xhr) => {
+            console.log(((xhr.loaded / xhr.total) * 100) + '% loaded');
+        },
+        (error) => {
+            console.error('Error loading model.', error);
+        }
+    );
+
+    /**
+     * Set up camera controller:
+     */
+
+    const mouseLookController = new MouseLookController(camera);
+
+    // We attach a click lister to the canvas-element so that we can request a pointer lock.
+    // https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
+    const canvas = renderer.domElement;
+
+    canvas.addEventListener('click', () => {
+        canvas.requestPointerLock();
+    });
+
+    let yaw = 0;
+    let pitch = 0;
+    const mouseSensitivity = 0.001;
+
+    function updateCamRotation(event) {
+        yaw += event.movementX * mouseSensitivity;
+        pitch += event.movementY * mouseSensitivity;
+    }
+
+    document.addEventListener('pointerlockchange', () => {
+        if (document.pointerLockElement === canvas) {
+            canvas.addEventListener('mousemove', updateCamRotation, false);
+        } else {
+            canvas.removeEventListener('mousemove', updateCamRotation, false);
+        }
+    });
+
+    let move = {
+        forward: false,
+        backward: false,
+        left: false,
+        right: false,
+        speed: 0.01
+    };
+
+    window.addEventListener('keydown', (e) => {
+        if (e.code === 'KeyW') {
+            move.forward = true;
+            e.preventDefault();
+        } else if (e.code === 'KeyS') {
+            move.backward = true;
+            e.preventDefault();
+        } else if (e.code === 'KeyA') {
+            move.left = true;
+            e.preventDefault();
+        } else if (e.code === 'KeyD') {
+            move.right = true;
+            e.preventDefault();
+        }
+    });
+
+    window.addEventListener('keyup', (e) => {
+        if (e.code === 'KeyW') {
+            move.forward = false;
+            e.preventDefault();
+        } else if (e.code === 'KeyS') {
+            move.backward = false;
+            e.preventDefault();
+        } else if (e.code === 'KeyA') {
+            move.left = false;
+            e.preventDefault();
+        } else if (e.code === 'KeyD') {
+            move.right = false;
+            e.preventDefault();
+        }
+    });
+
+    const velocity = new Vector3(0.0, 0.0, 0.0);
+
+    let then = performance.now();
+    function loop(now) {
+
+        const delta = now - then;
+        then = now;
+
+        const moveSpeed = move.speed * delta;
+
+        velocity.set(0.0, 0.0, 0.0);
+
+        if (move.left) {
+            velocity.x -= moveSpeed;
+        }
+
+        if (move.right) {
+            velocity.x += moveSpeed;
+        }
+
+        if (move.forward) {
+            velocity.z -= moveSpeed;
+        }
+
+        if (move.backward) {
+            velocity.z += moveSpeed;
+        }
+
+        // update controller rotation.
+        mouseLookController.update(pitch, yaw);
+        yaw = 0;
+        pitch = 0;
+
+        // apply rotation to velocity vector, and translate moveNode with it.
+        velocity.applyQuaternion(camera.quaternion);
+        camera.position.add(velocity);
+
+        // render scene:
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(loop);
+
+    };
+
+    loop(performance.now());
+
+}
+
+main(); // Start application
